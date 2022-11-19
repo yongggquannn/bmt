12e89c061ce5b8214055adc76dc90bc15de8c016
ShawnCsj <csjshawn@gmail.com>
2022-11-20 00:33:00 +0800

    Added template files

--- pages/index.vue
+++ pages/index.vue
@@ -1,86 +1,11 @@
 <template>
   <div>
-    <h1 class="text-3xl m-10 mb-4 font-bold">Happenings</h1>
-    <p class="ml-10 mb-7">
-      Stay in the know on the latest happenings & buzz in church!
-    </p>
-
-    <!-- ############# PART 2: PROPS & DATA!! ############## -->
-    <!-- ###   Create your card using the 'item' below   ### -->
-    <!-- ###   Use the given CSS or create your own :)   ### -->
-    <!-- ################################################### -->
-    <div class="max-w-[1440px] flex flex-wrap justify-center">
-      <Card :item="titleCaps" />
-
-      <!-- ############# PART 3: VUE DIRECTIVES!! ############## -->
-      <!-- ###          Edit your Card component to          ### -->
-      <!-- ###      create a list of cards using 'items'     ### -->
-      <!-- ##################################################### -->
-
-      <!-- <Card v-for="item in items" :key="item.id" :item="item" class="mb-5" /> -->
-    </div>
+    <Tutorial />
   </div>
 </template>
 
 <script>
-import Card from '~/components/Card.vue'
 export default {
   name: 'IndexPage',
-  component: { Card },
-  data() {
-    return {
-      item: {
-        id: '1',
-        title1: 'Connect a Youth #HOGC21',
-        title2: 'Connect a youth',
-        description: 'Bringing a youth? Let us know! We’re here to help!',
-        image: '',
-      },
-      items: [
-        {
-          id: '1',
-          title1: 'CONNECT A YOUTH #HOGC21',
-          title2: 'Connect a youth',
-          description: 'Bringing a youth? Let us know! We’re here to help!',
-          image: '',
-        },
-        {
-          id: '2',
-          title1: 'GET INVOLVED #HOGC21',
-          title2: 'Get involved!',
-          description: 'We need youth & we need YOU! Let’s reach down!',
-          image: '',
-        },
-        {
-          id: '3',
-          title1: '',
-          title2: 'HOGC21 Ideas',
-          description: 'Want to do something but not sure how? Fret not!',
-          image: '~/assets/images/hogc21.png',
-        },
-        {
-          id: '4',
-          title1: '',
-          title2: 'Youth Events',
-          description: 'Calling all young at heart',
-          image: '~/assets/images/youth-events.png',
-        },
-        {
-          id: '5',
-          title1: '',
-          title2: 'HeartKidz',
-          description: 'Every Sat 5-7pm & Sun 1.30-3.30pm',
-          image: '~/assets/images/heartkidz.png',
-        },
-        {
-          id: '6',
-          title1: '',
-          title2: 'Service Replay',
-          description: 'No FOMO! Sat 1.30pm & Sun 5pm',
-          image: '~/assets/images/service-replay.png',
-        },
-      ],
-    }
-  },
 }
 </script>
--- templates/index.vue
+++ templates/index.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
+    <div
+      class="flex bg-gradient-to-r from-[#FFE543] to-[#FFCD19] p-7 pl-10 mb-7"
+    >
+      <!-- ############# PART 1: PROJ STRUCTURE!! ############## -->
+      <!-- ###      Add in HOGC Logo into assets folder,     ### -->
+      <!-- ###      and add the file path in the img tag     ### -->
+      <!-- ##################################################### -->
+
+      <img class="mr-5" src="??" />
+      <h3 class="text-2xl font-bold">HOGC One</h3>
+    </div>
     <h1 class="text-3xl m-10 mb-4 font-bold">Happenings</h1>
     <p class="ml-10 mb-7">
       Stay in the know on the latest happenings & buzz in church!
@@ -9,30 +20,21 @@
     <!-- ###   Create your card using the 'item' below   ### -->
     <!-- ###   Use the given CSS or create your own :)   ### -->
     <!-- ################################################### -->
+
     <div class="max-w-[1440px] flex flex-wrap justify-center">
-      <Card :item="titleCaps" />
-
-      <!-- ############# PART 3: VUE DIRECTIVES!! ############## -->
-      <!-- ###          Edit your Card component to          ### -->
-      <!-- ###      create a list of cards using 'items'     ### -->
-      <!-- ##################################################### -->
-
-      <!-- <Card v-for="item in items" :key="item.id" :item="item" class="mb-5" /> -->
+      <!-- Add Card component here!  -->
     </div>
+
+    <!-- ############# PART 3: VUE DIRECTIVES!! ############## -->
+    <!-- ###          Edit your Card component to          ### -->
+    <!-- ###      create a list of cards using 'items'     ### -->
+    <!-- ##################################################### -->
   </div>
 </template>
 
 <script>
-import Card from '~/components/Card.vue'
 export default {
   name: 'IndexPage',
-  component: { Card },
-  computed: {
-    titleCaps() {
-      this.item.title1 = this.item.title1.toUpperCase()
-      return item
-    },
-  },
   data() {
     return {
       item: {
@@ -42,50 +44,6 @@ export default {
         description: 'Bringing a youth? Let us know! We’re here to help!',
         image: '',
       },
-      items: [
-        {
-          id: '1',
-          title1: 'CONNECT A YOUTH #HOGC21',
-          title2: 'Connect a youth',
-          description: 'Bringing a youth? Let us know! We’re here to help!',
-          image: '',
-        },
-        {
-          id: '2',
-          title1: 'GET INVOLVED #HOGC21',
-          title2: 'Get involved!',
-          description: 'We need youth & we need YOU! Let’s reach down!',
-          image: '',
-        },
-        {
-          id: '3',
-          title1: '',
-          title2: 'HOGC21 Ideas',
-          description: 'Want to do something but not sure how? Fret not!',
-          image: '~/assets/images/hogc21.png',
-        },
-        {
-          id: '4',
-          title1: '',
-          title2: 'Youth Events',
-          description: 'Calling all young at heart',
-          image: '~/assets/images/youth-events.png',
-        },
-        {
-          id: '5',
-          title1: '',
-          title2: 'HeartKidz',
-          description: 'Every Sat 5-7pm & Sun 1.30-3.30pm',
-          image: '~/assets/images/heartkidz.png',
-        },
-        {
-          id: '6',
-          title1: '',
-          title2: 'Service Replay',
-          description: 'No FOMO! Sat 1.30pm & Sun 5pm',
-          image: '~/assets/images/service-replay.png',
-        },
-      ],
     }
   },
 }
